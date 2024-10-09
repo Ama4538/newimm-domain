5bd6b91e67594d7399ca3fbde6ea61634994440a
Kevin Ly <Voldableprism09@outlook.com>
2024-10-09 18:58:56 -0400

    Added method templates
    Added methods for the immigrant class from the class diagram from iteration 2

--- src/main/java/edu/gmu/cs321/Immigrant.java
+++ src/main/java/edu/gmu/cs321/Immigrant.java
@@ -1,5 +1,31 @@
 package edu.gmu.cs321;
 
+import java.util.List;
+
 public class Immigrant {
+
+    // Submit a request form
+    public void submitRequest() {
+        // default return
+        return;
+    }
+
+    // View the request status of the form
+    public String viewRequestStatus() {
+        // default return
+        return '';
+    }
+
+    // View Document History
+    public List<> viewDocumentHistory() {
+        // default return
+        return null;
+    }
+
+    // Received Notification
+    public void receivedNotifications() {
+        // default return
+        return;
+    }
 
 }
